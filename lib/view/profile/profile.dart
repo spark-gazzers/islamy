c559de6f581fdde7989177ac18be8c90ddf41a07
Yousuf M.Hammad <yousuf.m.hammad@gmail.com>
2022-03-25 21:38:30 +0200

    added profile page

---
+++ lib/view/profile/profile.dart
@@ -0,0 +1,100 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:iconsax/iconsax.dart';
+import 'package:islamy/generated/l10n/l10n.dart';
+import 'package:islamy/theme.dart';
+import 'package:islamy/utils/helper.dart';
+import 'package:islamy/utils/store.dart';
+import 'package:islamy/view/common/rounded_icon.dart';
+
+class ProfileScreen extends StatelessWidget {
+  const ProfileScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return CupertinoPageScaffold(
+      navigationBar: CupertinoNavigationBar(
+        brightness: Brightness.dark,
+        middle: Text(S.of(context).profile),
+      ),
+      backgroundColor: ThemeBuilder.greyBackgroundColor(context),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          const SizedBox(height: 91.0),
+          ClipOval(
+            child: Container(
+              color: Colors.black,
+              width: 100,
+              height: 100,
+            ),
+          ),
+          const Padding(
+            padding: EdgeInsets.only(top: 16, bottom: 24.0),
+            child: Text(
+              'Ahmed Ali',
+              style: TextStyle(
+                fontSize: 18.0,
+                fontWeight: FontWeight.w600,
+                color: Colors.black,
+              ),
+            ),
+          ),
+          Container(
+            margin: const EdgeInsets.symmetric(horizontal: 28.0),
+            decoration: const BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.all(Radius.circular(16)),
+            ),
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                ValueListenableBuilder(
+                  valueListenable: Store.muteNotficationListner,
+                  builder: (_, __, ___) {
+                    return ListTile(
+                      leading: const RoundedIcon(Iconsax.notification),
+                      title: Text(S.of(context).mute_notifications),
+                      trailing: CupertinoSwitch(
+                          value: Store.muteNotfication,
+                          onChanged: (_) {
+                            Store.muteNotfication = !Store.muteNotfication;
+                          }),
+                      onTap: () {
+                        Store.muteNotfication = !Store.muteNotfication;
+                      },
+                    );
+                  },
+                ),
+                ValueListenableBuilder(
+                  valueListenable: Store.localeListner,
+                  builder: (_, __, ___) {
+                    // CountryCode.fromCountryCode('countryCode');
+                    // print(json.encode(
+                    //     LocaleNamesLocalizationsDelegate.nativeLocaleNames));
+                    // return Container();
+                    return ListTile(
+                      leading: const RoundedIcon(Iconsax.global),
+                      title: Text(S.of(context).language),
+                      trailing: Text(
+                        Helper.localization.nameOf(Store.locale),
+                        style: TextStyle(
+                          color: Theme.of(context).primaryColor,
+                          fontSize: 14.0,
+                          fontWeight: FontWeight.w700,
+                        ),
+                      ),
+                      onTap: () {
+                        Navigator.pushNamed(context, 'select_language');
+                      },
+                    );
+                  },
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
