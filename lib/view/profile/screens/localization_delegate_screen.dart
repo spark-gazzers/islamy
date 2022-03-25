e5c8c6fe3086d67ef1d2c142a6d48be071c6c716
Yousuf M.Hammad <yousuf.m.hammad@gmail.com>
2022-03-25 21:39:22 +0200

    added select locale screen

---
+++ lib/view/profile/screens/localization_delegate_screen.dart
@@ -0,0 +1,51 @@
+import 'package:flag/flag.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:islamy/generated/l10n/l10n.dart';
+import 'package:islamy/utils/helper.dart';
+import 'package:islamy/utils/store.dart';
+
+class LocalizationDelegateScreen extends StatelessWidget {
+  const LocalizationDelegateScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return CupertinoPageScaffold(
+      navigationBar: CupertinoNavigationBar(
+        previousPageTitle: S.of(context).profile,
+        middle: Text(S.of(context).select_language),
+        brightness: Brightness.dark,
+      ),
+      child: ListView.builder(
+        itemCount: S.delegate.supportedLocales.length,
+        itemBuilder: (context, index) {
+          final bool isSelected = Helper.localization
+              .equals(S.delegate.supportedLocales[index], Store.locale);
+          return ListTile(
+            selected: isSelected,
+            leading: Container(
+              decoration: const BoxDecoration(
+                borderRadius: BorderRadius.all(Radius.circular(6.0)),
+              ),
+              clipBehavior: Clip.hardEdge,
+              child: Flag.fromString(
+                S.delegate.supportedLocales[index].countryCode ?? 'ksa',
+                fit: BoxFit.cover,
+                height: 30,
+                width: 35,
+              ),
+            ),
+            title: Text(
+              Helper.localization.nameOf(S.delegate.supportedLocales[index]),
+            ),
+            trailing: isSelected ? const Icon(Icons.done_all) : null,
+            onTap: () {
+              Store.locale = S.delegate.supportedLocales[index];
+              Navigator.pop(context);
+            },
+          );
+        },
+      ),
+    );
+  }
+}
