df40ea5de3dd0c3868949cdf5de194778e132410
Yousuf M.Hammad <yousuf.m.hammad@gmail.com>
2022-03-27 19:18:02 +0200

    added reader settings

--- lib/view/profile/screens/quran_settings.dart
+++ lib/view/profile/screens/quran_settings.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:islamy/generated/l10n/l10n.dart';
+import 'package:islamy/utils/store.dart';
 
 class QuranSettingsScreen extends StatefulWidget {
   const QuranSettingsScreen({Key? key}) : super(key: key);
@@ -19,51 +21,89 @@ class _QuranSettingsScreenState extends State<QuranSettingsScreen> {
         brightness: Brightness.dark,
       ),
       backgroundColor: Theme.of(context).colorScheme.surfaceVariant,
-      child: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: CupertinoFormSection(
-          header: Text(S.of(context).quran_reader),
-          children: [
-            ListTile(
-              onTap: () {
-                Navigator.pushNamed(context, 'select_text_quran');
-              },
-              title: Text(S.of(context).text_edition),
-              trailing: const Icon(CupertinoIcons.forward),
-            ),
-            ListTile(
-              onTap: () {
-                Navigator.pushNamed(context, 'select_audio_quran');
-              },
-              title: Text(S.of(context).audio_edition),
-              trailing: const Icon(CupertinoIcons.forward),
-            ),
-            ListTile(
-              onTap: () {
-                Navigator.pushNamed(context, 'select_interpretation_quran');
-              },
-              title: Text(S.of(context).interpretation_edition),
-              trailing: const Icon(CupertinoIcons.forward),
-            ),
-            GestureDetector(
-              child: ListTile(
-                onTap: () {
-                  Navigator.pushNamed(context, 'select_translation_quran');
-                },
-                title: Text(S.of(context).translation_edition),
-                trailing: const Icon(CupertinoIcons.forward),
+      child: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            children: [
+              CupertinoFormSection(
+                header: Text(S.of(context).quran_editions),
+                children: [
+                  ListTile(
+                    onTap: () {
+                      Navigator.pushNamed(context, 'select_text_quran');
+                    },
+                    title: Text(S.of(context).text_edition),
+                    trailing: const Icon(CupertinoIcons.forward),
+                  ),
+                  ListTile(
+                    onTap: () {
+                      Navigator.pushNamed(context, 'select_audio_quran');
+                    },
+                    title: Text(S.of(context).audio_edition),
+                    trailing: const Icon(CupertinoIcons.forward),
+                  ),
+                  ListTile(
+                    onTap: () {
+                      Navigator.pushNamed(
+                          context, 'select_interpretation_quran');
+                    },
+                    title: Text(S.of(context).interpretation_edition),
+                    trailing: const Icon(CupertinoIcons.forward),
+                  ),
+                  GestureDetector(
+                    child: ListTile(
+                      onTap: () {
+                        Navigator.pushNamed(
+                            context, 'select_translation_quran');
+                      },
+                      title: Text(S.of(context).translation_edition),
+                      trailing: const Icon(CupertinoIcons.forward),
+                    ),
+                  ),
+                  GestureDetector(
+                    child: ListTile(
+                      onTap: () {
+                        Navigator.pushNamed(
+                            context, 'select_transliteration_quran');
+                      },
+                      title: Text(S.of(context).transliteration_edition),
+                      trailing: const Icon(CupertinoIcons.forward),
+                    ),
+                  ),
+                ],
               ),
-            ),
-            GestureDetector(
-              child: ListTile(
-                onTap: () {
-                  Navigator.pushNamed(context, 'select_transliteration_quran');
-                },
-                title: Text(S.of(context).transliteration_edition),
-                trailing: const Icon(CupertinoIcons.forward),
-              ),
-            ),
-          ],
+              CupertinoFormSection(
+                header: Text(S.of(context).quran_reader),
+                children: [
+                  ValueListenableBuilder(
+                    valueListenable: Store.shouldReadBasmlaOnSelectionListner,
+                    builder: (_, __, ___) {
+                      return ListTile(
+                        title:
+                            Text(S.of(context).should_read_basmla_on_selection),
+                        subtitle: Text(S
+                            .of(context)
+                            .note_this_will_not_stop_the_reader_from_reading_the_basmala_on_the_start_of_the_surah),
+                        onTap: () {
+                          HapticFeedback.lightImpact();
+                          Store.shouldReadBasmlaOnSelection =
+                              !Store.shouldReadBasmlaOnSelection;
+                        },
+                        trailing: CupertinoSwitch(
+                          value: Store.shouldReadBasmlaOnSelection,
+                          onChanged: (_) {
+                            Store.shouldReadBasmlaOnSelection =
+                                !Store.shouldReadBasmlaOnSelection;
+                          },
+                        ),
+                      );
+                    },
+                  ),
+                ],
+              )
+            ],
+          ),
         ),
       ),
     );
