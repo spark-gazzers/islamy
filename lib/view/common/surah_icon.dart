9226ba525bed4e1ed8c174b5fda404d472933fa9
Yousuf M.Hammad <yousuf.m.hammad@gmail.com>
2022-03-28 13:04:54 +0200

    adding common design icon for surahs

---
+++ lib/view/common/surah_icon.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+class SurahIcon extends StatelessWidget {
+  final int number;
+  final Color color;
+  const SurahIcon({Key? key, required this.number, required this.color})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return IntrinsicWidth(
+      child: Stack(
+        children: [
+          Align(
+            alignment: Alignment.center,
+            child: SvgPicture.asset(
+              'assets/icons/surah_icon.svg',
+              color: color,
+            ),
+          ),
+          Align(
+            alignment: Alignment.center,
+            child: Text(
+              number.toString(),
+              style: TextStyle(
+                fontSize: 12,
+                color: color,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
