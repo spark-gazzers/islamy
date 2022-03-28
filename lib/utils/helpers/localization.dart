550c07c51df37dd4dfd3a3af2e9b33b941ba0c1f
Yousuf M.Hammad <yousuf.m.hammad@gmail.com>
2022-03-28 13:04:24 +0200

    adding verse symbol

--- lib/utils/helpers/localization.dart
+++ lib/utils/helpers/localization.dart
@@ -24,4 +24,44 @@ class _LocalizationHelper {
   bool equals(Locale l1, Locale l2) =>
       l1.toLanguageTag().replaceAll('-', '_').toLowerCase() ==
       l2.toLanguageTag().replaceAll('-', '_').toLowerCase();
+
+  String getVerseEndSymbol(int verseNumber) {
+    var arabicNumeric = '';
+    var digits = verseNumber.toString().split("").toList();
+
+    for (var e in digits) {
+      if (e == "0") {
+        arabicNumeric += "٠";
+      }
+      if (e == "1") {
+        arabicNumeric += "۱";
+      }
+      if (e == "2") {
+        arabicNumeric += "۲";
+      }
+      if (e == "3") {
+        arabicNumeric += "۳";
+      }
+      if (e == "4") {
+        arabicNumeric += "۴";
+      }
+      if (e == "5") {
+        arabicNumeric += "۵";
+      }
+      if (e == "6") {
+        arabicNumeric += "۶";
+      }
+      if (e == "7") {
+        arabicNumeric += "۷";
+      }
+      if (e == "8") {
+        arabicNumeric += "۸";
+      }
+      if (e == "9") {
+        arabicNumeric += "۹";
+      }
+    }
+
+    return '\u06dd' + arabicNumeric.toString();
+  }
 }
