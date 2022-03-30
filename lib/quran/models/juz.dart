98cb99dd82b519e4901dd54e669aae22d17e4e83
Yousuf M.Hammad <yousuf.m.hammad@gmail.com>
2022-03-30 15:51:02 +0200

    adding juz data model

---
+++ lib/quran/models/juz.dart
@@ -0,0 +1,47 @@
+import 'dart:convert' show json;
+
+import 'package:islamy/utils/store.dart';
+
+class Juz {
+  Juz({
+    required this.index,
+    required this.name,
+    required this.englishName,
+    required this.surahsRange,
+  });
+
+  final int index;
+  final String name;
+  final String englishName;
+  final SurahsRange surahsRange;
+  String get translatedName =>
+      Store.locale.languageCode.startsWith('ar') ? name : englishName;
+  String get otherName =>
+      !Store.locale.languageCode.startsWith('ar') ? name : englishName;
+  static List<Juz> listFromRawJson(String str) =>
+      (json.decode(str) as List).map((e) => Juz.fromJson(e)).toList();
+
+  factory Juz.fromJson(Map<String, dynamic> json) => Juz(
+        index: json["index"],
+        name: json["name"],
+        englishName: json["englishName"],
+        surahsRange: SurahsRange.fromJson(json["surahs_range"]),
+      );
+  bool containsSurah(int number) =>
+      number >= surahsRange.start && number <= surahsRange.end;
+}
+
+class SurahsRange {
+  SurahsRange({
+    required this.start,
+    required this.end,
+  });
+
+  final int start;
+  final int end;
+
+  factory SurahsRange.fromJson(Map<String, dynamic> json) => SurahsRange(
+        start: json["start"],
+        end: json["end"],
+      );
+}
