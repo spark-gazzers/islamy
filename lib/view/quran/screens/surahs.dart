34bc42b211ae5ebc07c3c08109caeeb9d1ce70b2
Yousuf M.Hammad <yousuf.m.hammad@gmail.com>
2022-04-15 02:39:47 +0200

    changing the name of holly quran model file

--- lib/view/quran/screens/surahs.dart
+++ lib/view/quran/screens/surahs.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:islamy/generated/l10n/l10n.dart';
 import 'package:islamy/quran/models/enums.dart';
 import 'package:islamy/quran/models/surah.dart';
-import 'package:islamy/quran/models/text_quran.dart';
+import 'package:islamy/quran/models/the_holy_quran.dart';
 import 'package:islamy/quran/quran_player_controller.dart';
 import 'package:islamy/quran/store/quran_store.dart';
 import 'package:islamy/view/common/surah_icon.dart';
