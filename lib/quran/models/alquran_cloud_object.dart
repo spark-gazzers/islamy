12205a2ca020f243efa433c1806d2b5a207aac07
Yousuf M.Hammad <yousuf.m.hammad@gmail.com>
2022-04-15 03:20:07 +0200

    changing fields to getters to remove fmt warnings

--- lib/quran/models/alquran_cloud_object.dart
+++ lib/quran/models/alquran_cloud_object.dart
@@ -1,8 +1,8 @@
 import 'package:islamy/utils/store.dart';
 
 abstract class AlquranCloudObject {
-  late final String name;
-  late final String englishName;
+  String get name;
+  String get englishName;
 
   String get localizedName =>
       Store.locale.toLanguageTag().startsWith('ar') ? name : englishName;
