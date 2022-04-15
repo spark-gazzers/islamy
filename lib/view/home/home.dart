e6ff54e305209e848e4f56f9a258e2f1e4f168d0
Yousuf M.Hammad <yousuf.m.hammad@gmail.com>
2022-04-15 02:46:28 +0200

    adding home screen

--- lib/view/home/home.dart
+++ lib/view/home/home.dart
@@ -1,2 +1,15 @@
 import 'package:flutter/material.dart';
-// stat
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
