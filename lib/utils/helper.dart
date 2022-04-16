ee807f4153d15e91cadf5a6e5a6e3e97111fda13
Yousuf M.Hammad <yousuf.m.hammad@gmail.com>
2022-04-16 18:47:46 +0200

    adding translator helper

--- lib/utils/helper.dart
+++ lib/utils/helper.dart
@@ -8,15 +8,18 @@ import 'package:flutter/services.dart';
 part 'helpers/localization.dart';
 part 'helpers/messages.dart';
 part 'helpers/formatters.dart';
+part 'helpers/translator.dart';
 
 class Helper {
   const Helper._();
   static Future<void> init() async {
     await localization.init();
     await formatters.init();
+    await translator.init();
   }
 
   static const _LocalizationHelper localization = _LocalizationHelper.instance;
   static const _MessagesHelper messages = _MessagesHelper.instance;
   static const _Formatters formatters = _Formatters.instance;
+  static const _Translator translator = _Translator.instance;
 }
---
+++ lib/utils/helpers/translator.dart
@@ -0,0 +1,7 @@
+part of helper;
+
+class _Translator {
+  const _Translator._();
+  static const _Translator instance = _Translator._();
+  Future<void> init() async {}
+}
