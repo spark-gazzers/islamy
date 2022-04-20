268715f01d1b6d92786f5b29f2cda5edbe0fdbad
Yousuf M.Hammad <yousuf.m.hammad@gmail.com>
2022-04-21 01:12:23 +0200

    moving intializers to splash screen.

--- lib/main.dart
+++ lib/main.dart
@@ -1,39 +1,14 @@
 import 'package:country_code_picker/country_localizations.dart';
-import 'package:ffmpeg_kit_flutter_min/ffmpeg_kit_config.dart';
-import 'package:ffmpeg_kit_flutter_min/log_redirection_strategy.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:islamy/generated/l10n/l10n.dart';
-import 'package:islamy/quran/models/the_holy_quran.dart';
-import 'package:islamy/quran/quran_manager.dart';
 import 'package:islamy/theme.dart';
-import 'package:islamy/utils/api/api_handler.dart';
-import 'package:islamy/utils/helper.dart';
 import 'package:islamy/utils/routes.dart';
-import 'package:islamy/utils/store.dart';
 
 Future<void> main(List<String> args) async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await init();
   // return live.main();
   runApp(const IslamyApp());
-}
-
-/// The main app initializer which loads every static service in the app.
-///
-/// Note.
-/// Depending on the user this can take from 3 seconds upto 7 in somecase cause
-/// of the load and the proccess of many [TheHolyQuran] objects.
-/// So UX wise, those initializer should never be called outside splash screen
-Future<void> init() async {
-  await Store.init();
-  ApiHandler.init();
-  await QuranManager.init();
-  await Helper.init();
-  FFmpegKitConfig.setLogRedirectionStrategy(
-    LogRedirectionStrategy.neverPrintLogs,
-  );
 }
 
 ///The main application [WidgetsApp]
@@ -64,7 +39,7 @@ class IslamyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       supportedLocales: S.delegate.supportedLocales,
       onGenerateRoute: Routes.onGenerateRoute,
-      initialRoute: 'main',
+      initialRoute: 'splash',
     );
   }
 }
--- lib/view/splash/splash.dart
+++ lib/view/splash/splash.dart
@@ -1,17 +1,60 @@
+import 'package:ffmpeg_kit_flutter_min/ffmpeg_kit_config.dart';
+import 'package:ffmpeg_kit_flutter_min/log_redirection_strategy.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:islamy/quran/models/the_holy_quran.dart';
+import 'package:islamy/quran/quran_manager.dart';
+import 'package:islamy/utils/api/api_handler.dart';
+import 'package:islamy/utils/helper.dart';
+import 'package:islamy/utils/store.dart';
 
 /// The root [Route] screen that loads the static intializers of the app.
-//TODO(psyonixFx): move the intializers here.
-class SplashScreen extends StatelessWidget {
+class SplashScreen extends StatefulWidget {
   const SplashScreen({Key? key}) : super(key: key);
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    init();
+    super.initState();
+  }
+
+  /// The main app initializer which loads every static service in the app.
+  ///
+  /// Note.
+  /// Depending on the user this can take from 3 seconds upto 7 in somecase
+  /// cause of the load and the proccess of many [TheHolyQuran] objects.
+  /// So UX wise, those initializer should never be called outside splash screen
+  Future<void> init() async {
+    final now = DateTime.now();
+    await Store.init();
+    ApiHandler.init();
+    await QuranManager.init();
+    await Helper.init();
+    FFmpegKitConfig.setLogRedirectionStrategy(
+      LogRedirectionStrategy.neverPrintLogs,
+    );
+    final Duration length = now.difference(DateTime.now()).abs();
+    print('done after ${length.inSeconds}');
+  }
+
+  // void downloadAll() async {
+  //   final List<Edition> all = QuranStore.listAudioEditions();
+  //   for (var i = 0; i < all.length; i++) {
+  //     await QuranManager.downloadQuran(edition: all[i]);
+  //     print('$i out of ${all.length}');
+  //   }
+  //   print('done');
+  // }
 
   @override
   Widget build(BuildContext context) {
     SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle.dark);
-    Future<void>.delayed(const Duration(seconds: 2)).then(
-      (_) => Navigator.pushReplacementNamed(context, 'on_boarding'),
-    );
+
     return const Material(
       elevation: 0,
       child: Center(
