094b7739a143a794ecfa922450ecfbf0e28e914e
Yousuf M.Hammad <yousuf.m.hammad@gmail.com>
2022-04-22 16:19:37 +0200

    updating the splitter to accomodate to the Sajda surah bug

--- lib/quran/tajweed/splitter.dart
+++ lib/quran/tajweed/splitter.dart
@@ -34,22 +34,43 @@ class AyahTajweedSplitter {
       } else {
         // Add the preceduer as plane
         spans.add(TextSpan(text: text.substring(index, newIndex)));
+        // Using try/catch cause the letter may be in bracket without a rule arrounde it
+        //
+        //
+        // This exception by far happend only for one Ayah :
+        // Edition = quran-tajweed
+        // Surah : Al-Sajda
+        // Ayah's number in surah = 3
         // Exracting the [TajweedRule] id
         final String id = text.substring(newIndex, newIndex + 2);
-        // Respected tajweed rule from the id
-        final TajweedRule rule = TajweedRule.rules
-            .singleWhere((TajweedRule element) => element.id == id);
         final String letterToPatch = text.substring(
           text.indexOf('[', newIndex + 1) + 1,
           text.indexOf(']', newIndex),
         );
-        // Adding the extracted letter with the [TajweedRule] color
-        spans.add(
-          TextSpan(
-            text: letterToPatch,
-            style: TextStyle(color: rule.color),
-          ),
-        );
+
+        try {
+          // Respected tajweed rule from the id
+          final TajweedRule rule = TajweedRule.rules
+              .singleWhere((TajweedRule element) => element.id == id);
+          // Adding the extracted letter with the [TajweedRule] color
+          spans.add(
+            TextSpan(
+              text: letterToPatch,
+              style: TextStyle(color: rule.color),
+            ),
+          );
+        } catch (e) {
+          // If the tajweed rule not found then just skip it and
+          // add it as normal text
+          spans.add(
+            TextSpan(
+              text: String.fromCharCode(
+                // The letter index
+                text.codeUnitAt(text.indexOf(']', newIndex) - 1),
+              ),
+            ),
+          );
+        }
         // start the new loop from the end of the patched letter
         newIndex = text.indexOf(']', newIndex);
       }
