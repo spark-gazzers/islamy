18c98d17b5789d2d94594fd77d8030ef03a8ab8c
Yousuf M.Hammad <yousuf.mg.hammad@gmail.com>
2023-10-03 16:03:44 +0300

    inherting from AudioServiceActivity in the main activity

--- android/app/src/main/kotlin/app/islamy/MainActivity.kt
+++ android/app/src/main/kotlin/app/islamy/MainActivity.kt
@@ -1,6 +1,7 @@
 package app.islamy
 
 import io.flutter.embedding.android.FlutterActivity
+import com.ryanheise.audioservice.AudioServiceActivity;
 
-class MainActivity: FlutterActivity() {
+class MainActivity: AudioServiceActivity() {
 }
