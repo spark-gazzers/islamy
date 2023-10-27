b78ece9d854b3e4e5725faff00d9d661198d5587
Yousuf M.Hammad <yousuf.mg.hammad@gmail.com>
2023-10-27 20:37:53 +0200

    added language to the hadeeth model

--- lib/engines/hadeeth/models/hadeeth.dart
+++ lib/engines/hadeeth/models/hadeeth.dart
@@ -9,16 +9,19 @@ class Hadeeth extends HiveObject {
     required this.id,
     required this.title,
     required this.translations,
+    required this.languageCode,
   });
 
-  factory Hadeeth.fromRawJson(String str) =>
-      Hadeeth.fromJson(json.decode(str) as Map<String, dynamic>);
+  factory Hadeeth.fromRawJson(String str, String languageCode) =>
+      Hadeeth.fromJson(json.decode(str) as Map<String, dynamic>, languageCode);
 
-  factory Hadeeth.fromJson(Map<String, dynamic> json) => Hadeeth(
+  factory Hadeeth.fromJson(Map<String, dynamic> json, String languageCode) =>
+      Hadeeth(
         id: json['id'] as String,
         title: json['title'] as String,
-        translations: List<String>.from(
-            (json['translations'] as List<dynamic>).map((x) => x as String)),
+        translations: List<String>.from((json['translations'] as List<dynamic>)
+            .map((dynamic x) => x as String)),
+        languageCode: languageCode,
       );
 
   @HiveField(0)
@@ -27,9 +30,16 @@ class Hadeeth extends HiveObject {
   final String title;
   @HiveField(2)
   final List<String> translations;
+  @HiveField(3)
+  final String languageCode;
 
-  static List<Hadeeth> listFrom(List<Map<String, dynamic>> json) =>
-      List<Hadeeth>.from(json.map<Hadeeth>(Hadeeth.fromJson));
+  static List<Hadeeth> listFrom(
+    List<Map<String, dynamic>> json,
+    String languageCode,
+  ) =>
+      List<Hadeeth>.from(json.map<Hadeeth>(
+        (Map<String, dynamic> e) => Hadeeth.fromJson(e, languageCode),
+      ));
 
   String toRawJson() => json.encode(toJson());
 
