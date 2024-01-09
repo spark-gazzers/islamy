27caf1f933157c125c4ba8ecfcf1a96598f4f4d9
Yousuf M.Hammad <yousuf.mg.hammad@gmail.com>
2024-01-09 16:04:44 +0400

    getting the hadeeths from the cloud and caching them

--- lib/view/hadeeth/hadeeths_screen.dart
+++ lib/view/hadeeth/hadeeths_screen.dart
@@ -4,6 +4,7 @@ import 'package:islamy/engines/hadeeth/hadeeth_manager.dart';
 import 'package:islamy/engines/hadeeth/models/hadeeth.dart';
 import 'package:islamy/engines/hadeeth/models/hadeeth_category.dart';
 import 'package:islamy/generated/l10n/l10n.dart';
+import 'package:islamy/view/common/loading_widget.dart';
 import 'package:islamy/view/hadeeth/hadeeth_tile.dart';
 import 'package:sliver_tools/sliver_tools.dart';
 
@@ -22,13 +23,6 @@ class _HadeethsScreenState extends State<HadeethsScreen> {
   final TextEditingController _searchController = TextEditingController();
   final FocusNode _searchNode = FocusNode();
 
-  List<Hadeeth> get hadeeths => HadeethStore.listHadeeths()
-      .where(
-        (Hadeeth hadeeth) =>
-            hadeeth.category == widget.category.id &&
-            hadeeth.languageCode == HadeethStore.settings.language.code,
-      )
-      .toList();
   @override
   Widget build(BuildContext context) {
     return CupertinoPageScaffold(
@@ -53,21 +47,90 @@ class _HadeethsScreenState extends State<HadeethsScreen> {
               ),
             ),
           ),
-          AnimatedBuilder(
-              animation: _searchController,
-              builder: (BuildContext context, Widget? child) {
-                return SliverList.builder(
-                  itemBuilder: (BuildContext context, int index) {
-                    if (index.isOdd) return const Divider();
-                    return HadeethTile(
-                      hadeeth: hadeeths[index ~/ 2],
+          ValueListenableBuilder<TextEditingValue>(
+            valueListenable: _searchController,
+            builder: (
+              BuildContext context,
+              TextEditingValue controller,
+              Widget? child,
+            ) {
+              return FutureBuilder<List<Hadeeth>>(
+                future: HadeethManager.listHadeeths(widget.category),
+                builder: (BuildContext context,
+                    AsyncSnapshot<List<Hadeeth>> snapshot) {
+                  Widget? widget;
+                  if (snapshot.hasData != true ||
+                      snapshot.connectionState != ConnectionState.done) {
+                    widget = const Center(
+                      child: LoadingWidget(),
                     );
-                  },
-                  itemCount: (hadeeths.length * 2) - 1,
-                );
-              }),
+                  }
+                  if (snapshot.hasError) widget = const Center();
+
+                  if (widget != null) {
+                    return SliverFillRemaining(
+                      child: widget,
+                    );
+                  }
+                  return _buildHadeethsTile(snapshot.data!);
+                },
+              );
+            },
+          ),
         ],
       ),
+    );
+  }
+
+  Widget _buildHadeethsTile(List<Hadeeth> hadeeths) {
+    final List<Hadeeth> filtered = hadeeths
+        .where((Hadeeth element) =>
+            element.title.contains(_searchController.text.trim()))
+        .toList();
+
+    if (filtered.isEmpty) {
+      return SliverFillRemaining(
+        child: Center(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 48),
+            child: Text.rich(
+              TextSpan(
+                style: Theme.of(context).textTheme.bodyLarge,
+                children: <InlineSpan>[
+                  TextSpan(text: '${S.of(context).no_hadeeth_found_with} "'),
+                  TextSpan(
+                    text: _searchController.text.trim(),
+                    style: TextStyle(
+                      color: Theme.of(context).colorScheme.error,
+                    ),
+                  ),
+                  TextSpan(text: '" ${S.of(context).in_the} "'),
+                  TextSpan(
+                    text: widget.category.title,
+                    style: TextStyle(
+                      color: Theme.of(context).colorScheme.tertiary,
+                    ),
+                  ),
+                  TextSpan(
+                    text:
+                        '" ${S.of(context).category_maybe_try_a_more_generic_search}',
+                  ),
+                ],
+              ),
+              textAlign: TextAlign.center,
+            ),
+          ),
+        ),
+      );
+    }
+    return SliverList.builder(
+      itemBuilder: (BuildContext context, int index) {
+        if (index.isOdd) return const Divider();
+        return HadeethTile(
+          hadeeth: filtered[index ~/ 2],
+        );
+      },
+      itemCount: (filtered.length * 2) - 1,
     );
   }
 }
